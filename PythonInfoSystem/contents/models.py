6df1fcc3e11bad4542510c6cc988f1b27cbfbe30
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-21 20:27:04 +0300

    Added a string method for better objects representation.

--- PythonInfoSystem/contents/models.py
+++ PythonInfoSystem/contents/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Topic(models.Model):
     topic = models.CharField(max_length=100)
     topic_text = models.TextField()
+    def __str__(self):
+        return self.topic_text
