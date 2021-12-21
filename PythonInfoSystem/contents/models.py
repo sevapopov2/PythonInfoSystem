270bf1abd41ea1025accdb2b92b2f51a8439bfef
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-21 20:35:11 +0300

    Fixed the str function for correct topics representation.

--- PythonInfoSystem/contents/models.py
+++ PythonInfoSystem/contents/models.py
@@ -4,4 +4,4 @@ class Topic(models.Model):
     topic = models.CharField(max_length=100)
     topic_text = models.TextField()
     def __str__(self):
-        return self.topic_text
+        return self.topic
