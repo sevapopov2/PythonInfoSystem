82e34760c5936a7a742318cd1dfcf1afd9299688
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-21 21:40:40 +0300

    A simple view with number print in function is added.

--- PythonInfoSystem/contents/views.py
+++ PythonInfoSystem/contents/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request):
     return HttpResponse("Добро пожаловать на страницу образовательной системы.")
+
+def topic_detail(request, topic_id):
+    return HttpResponse("You are looking at the topic %s." % topic_id)
