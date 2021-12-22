4f0b4bc58ee5dc236d9bf9cedcf0ef2bcecaf05c
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-22 13:44:06 +0300

    Connected an index template to views file.

--- PythonInfoSystem/contents/views.py
+++ PythonInfoSystem/contents/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Topic
 
 def index(request):
     topics_list = Topic.objects.all()
-    output = ', '.join([t.topic for t in topics_list])
-    return HttpResponse(output)
+    template = loader.get_template('contents/index.html')
+    context = {
+        'topics_list': topics_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def topic_detail(request, topic_id):
     return HttpResponse("You are looking at the topic %s." % topic_id)
