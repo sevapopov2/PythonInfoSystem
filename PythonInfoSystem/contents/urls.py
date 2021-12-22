593382fc93cca29ff840c0077b06ec6b0c27e8d9
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-22 21:04:12 +0300

    Namespace for contents app is added.

--- PythonInfoSystem/contents/urls.py
+++ PythonInfoSystem/contents/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'contents'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:topic_id>/', views.topic_detail, name='detail')
