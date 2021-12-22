c461079350c486ff55ae620d98f52fc76c313d99
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-22 16:05:57 +0300

    Try catche method is added for topic existance.

--- PythonInfoSystem/contents/views.py
+++ PythonInfoSystem/contents/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Topic
@@ -8,4 +9,8 @@ def index(request):
     return render(request, 'contents/index.html', context)
 
 def topic_detail(request, topic_id):
-    return HttpResponse("You are looking at the topic %s." % topic_id)
+    try:
+        topic = Topic.objects.get(pk=topic_id)
+    except Topic.DoesNotExist:
+        raise Http404("The topic doesn't exist.")
+    return render(request, 'contents/topic_detail.html', {'topic': topic})
