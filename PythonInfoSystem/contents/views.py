e6bd37a69b6b45733e248a2741b1f3efa1291bf1
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-22 12:24:21 +0300

    Added a simple view to display topics names on string.

--- PythonInfoSystem/contents/views.py
+++ PythonInfoSystem/contents/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Topic
 
 def index(request):
-    return HttpResponse("Добро пожаловать на страницу образовательной системы.")
+    topics_list = Topic.objects.all()
+    output = ', '.join([t.topic for t in topics_list])
+    return HttpResponse(output)
 
 def topic_detail(request, topic_id):
     return HttpResponse("You are looking at the topic %s." % topic_id)
