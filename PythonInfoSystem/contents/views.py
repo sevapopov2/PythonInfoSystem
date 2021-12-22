9713fb53eb69dd341046adbc4c72fb7da139f9ac
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-22 13:56:19 +0300

    Added a shortcut render function to write a shorter view representation.

--- PythonInfoSystem/contents/views.py
+++ PythonInfoSystem/contents/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Topic
 
 def index(request):
     topics_list = Topic.objects.all()
-    template = loader.get_template('contents/index.html')
-    context = {
-        'topics_list': topics_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'topics_list': topics_list}
+    return render(request, 'contents/index.html', context)
 
 def topic_detail(request, topic_id):
     return HttpResponse("You are looking at the topic %s." % topic_id)
